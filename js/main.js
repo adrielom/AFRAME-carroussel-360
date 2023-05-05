4f6fec79fc265edf621cf1a80b8532d84bdb3974
adrielom <adrielom.dev@gmail.com>
2023-05-04 21:40:19 -0300

    fix: camera movement bug. fixing the animation tween

--- js/main.js
+++ js/main.js
@@ -3,41 +3,38 @@ const easing = 'easeOutQuad';
 
 AFRAME.registerComponent('main', {
     init: function () {
-        console.log('first')
     }
 })
 
 AFRAME.registerComponent('clickable', {
     init: function () {
-        this.el.addEventListener('mouseenter', function (el) {
-            const startScale = { x: 1, y: 1, z: 1 };
-            const endScale = { x: 2, y: 2, z: 2 };
-            anime({
-                targets: startScale,
-                x: endScale.x,
-                y: endScale.y,
-                z: endScale.z,
-                duration: duration,
-                easing: easing,
-                update: function() {
-                    el.target.setAttribute('scale', startScale);
-                }
-            });
-        })
-        this.el.addEventListener('mouseleave', function (el) {
-            const startScale = { x: 1, y: 1, z: 1 };
-            const endScale = { x: 2, y: 2, z: 2 };
-            anime({
-                targets: endScale,
-                x: startScale.x,
-                y: startScale.y,
-                z: startScale.z,
-                duration: duration,
-                easing: easing,
-                update: function() {
-                    el.target.setAttribute('scale', endScale);
-                }
-            });
-        })
+        this.el.addEventListener('mouseenter', this.onMouseEnter.bind(this))
+        this.el.addEventListener('mouseleave', this.onMouseExit.bind(this))
+    },
+    onMouseEnter: function(event) {
+        if (event.target === this.el) return
+        const startScale = { x: 2, y: 2, z: 2 };
+        const endScale = { x: 1, y: 1, z: 1 };
+        this.animateScale(startScale, endScale)
+    },
+    onMouseExit: function(event) {
+        if (event.target === this.el) return
+        const startScale = { x: 1, y: 1, z: 1 };
+        const endScale = { x: 2, y: 2, z: 2 };
+        this.animateScale(startScale, endScale)
+    },
+    animateScale: function (startScale, endScale) {
+        target = this.el.target
+        anime({
+            targets: endScale,
+            x: startScale.x,
+            y: startScale.y,
+            z: startScale.z,
+            duration: duration,
+            easing: easing,
+            update: function() {
+                this.target.setAttribute('scale', endScale);
+            }
+        });
     }
 })
